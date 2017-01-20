cf5c4fec9f5b70dd5c68369bb6d524b13b9dd90d
skystar-p <wogus150@naver.com>
2017-01-20 13:53:06 +0900

    if_func added, and can return tree by dict

--- backend/core/rule/functions.py
+++ backend/core/rule/functions.py
@@ -1,5 +1,5 @@
 
-def make_func(function_name):
+def make_func(function_name, **kwargs):
     if function_name == 'and':
         return and_func
     elif function_name == 'or':
@@ -11,24 +11,30 @@ def make_func(function_name):
 
 
 def void_func(*pos):
-    def closure_void_func(*args):
+    def closure_void_func(*args, **kwargs):
         return True
     return closure_void_func
 
 
 def and_func(*pos):
-    def closure_and_func(*args):
+    def closure_and_func(*args, **kwargs):
         return all(args)
     return closure_and_func
 
 
 def or_func(*pos):
-    def closure_or_func(*args):
+    def closure_or_func(*args, **kwargs):
         return any(args)
     return closure_or_func
 
 
 def part_func(*pos):
-    def closure_part_func(*args):
+    def closure_part_func(*args, **kwargs):
         return pos[0] <= args.count(True)
     return closure_part_func
+
+
+def if_func(*pos, **metadata):
+    meta = metadata[pos[0]]
+    arg = pos[1]
+    return meta == arg
