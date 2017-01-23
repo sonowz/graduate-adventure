8dfa14bd88da3c8c62b5e93f5295f69bd239273f
dnsdhrj <dnsdhrj123@gmail.com>
2017-01-23 18:11:45 +0900

    Fixed logging issue & refactored API

--- backend/login/views.py
+++ backend/login/views.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
 from django.http.response import HttpResponse
 from django.shortcuts import redirect
+from django.conf import settings
 from rest_framework.views import APIView
 from core.crawler import crawl_credit, crawl_major
 from core.rule.tree import TreeLoader
 from core.rule.util import find_rule
-import os
-import logging
+import logging.config
 
-os.makedirs(os.getcwd() + '/log', exist_ok=True)
-logging.basicConfig(filename=os.getcwd() + '/log/loginview.log', level=logging.DEBUG)
+
+logging.config.dictConfig(settings.LOGGING)
+logger = logging.getLogger('backend')
+
+
+class ClientRenderedException(Exception):
+    pass
 
 
 def login_page(request):
-    logging.debug('Login page request with sessionid: ' + request.session.session_key)
+    logger.debug('Login page request with sessionid: ' + request.session.session_key)
     return HttpResponse("Hello World!")
 
 
@@ -23,29 +28,28 @@ class LoginRequest(APIView):
     #   - Used GET method instead of POST
     #   - Redirect to '/login/test' instead of '/main'
     def get(self, request, *args, **kwargs):
-        logging.debug('mySNU login request with sessionid: ' + str(request.session.session_key))
+        logger.debug('mySNU login request with sessionid: ' + str(request.session.session_key))
         user_id = request.GET.get('user_id', 'nid')
         password = request.GET.get('password', 'npw')
-        sugang_list = crawl_credit(user_id, password)
-        if len(sugang_list) == 0:
-            logging.error('Crawling failed.')
-            return HttpResponse('로그인에 실패했습니다.')
 
         try:
+            sugang_list = crawl_credit(user_id, password)
+            if len(sugang_list) == 0:
+                raise ClientRenderedException('로그인에 실패했습니다.')
             rule = self.get_rule(request)
             # tree =
-            TreeLoader(rule, sugang_list)
+            TreeLoader(rule, sugang_list, [])
             # do something with 'tree' here
+        # IOError should be handled within 'TreeLoader' later
         except IOError:
-            logging.error('No such file: ' + rule)
+            logger.error('No such file: ' + rule)
             pass
-        except Exception as e:
-            logging.error("Couldn't get rule file: " + e.args.msg)
-            return HttpResponse(e.args.msg)
+        except ClientRenderedException as e:
+            logger.error(e.args[0])
+            return HttpResponse(e.args[0])
 
         request.session['list'] = sugang_list
         request.session.set_expiry(600)
-        logging.debug('Redirecting to /main/...')
         return redirect('/login/test')
 
     def get_rule(self, request):
@@ -59,7 +63,7 @@ class LoginRequest(APIView):
                 ]
                 major_info = {x: request.GET[x] for x in arg}
             except KeyError:
-                raise Exception(msg='전공 정보가 없습니다.')
+                raise ClientRenderedException('전공 정보가 없습니다.')
         else:
             user_id = request.GET.get('user_id', 'nid')
             password = request.GET.get('password', 'npw')
