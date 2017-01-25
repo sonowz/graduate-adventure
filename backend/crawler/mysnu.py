fff9ac42bfdbed55d5f41d286a63fe84845da64a
Jhuni0123 <whdgnsdl887@gmail.com>
2017-01-25 14:25:10 +0900

    Merge & Fix mysnu module

--- backend/crawler/mysnu.py
+++ backend/crawler/mysnu.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 import logging.config
 from django.conf import settings
@@ -36,3 +37,83 @@ def login(userid, password):
 
         s.get('https://shine.snu.ac.kr/com/ssoLoginForSWAction.action')
         return s
+
+
+def crawl_credit(userid, password):
+    session = login(userid, password)
+    if session is None:
+        return None
+    with session as s:
+        url = 'https://shine.snu.ac.kr/uni/uni/scor/mrtr/findTabCumlMrksYyShtmClsfTtInq02.action'
+        params = {'cscLocale': 'ko_KR', 'strPgmCd': 'S030302'}
+        headers = {'Content-Type': 'application/extJs+sua; charset=UTF-8'}
+        payload = {
+            "SUN": {
+                "strSchyy": "2015",
+                "strShtmFg": "U000200001",
+                "strDetaShtmFg": "U000300001",
+                "strBdegrSystemFg": "U000100001",
+                "strFlag": "all"
+            }
+        }
+        grade_response = s.post(url, params=params, headers=headers, data=json.dumps(payload))
+        grade_response.raise_for_status()
+        grade_list = grade_response.json()['GRD_SCOR401']
+
+        semester_name = {
+            'U000200001U000300001': '1',
+            'U000200001U000300002': 'S',
+            'U000200002U000300001': '2',
+            'U000200002U000300002': 'W',
+        }
+
+        def refine(raw):
+            return {
+                'year': int(raw['schyy']),
+                'semester': semester_name[raw['shtmFg'] + raw['detaShtmFg']],
+                'code': raw['sbjtCd'],
+                'number': raw['ltNo'],
+                'title': raw['sbjtNm'],
+                'credit': raw['acqPnt'],
+                'grade': raw['mrksGrdCd'],
+                'category': raw['cptnSubmattFgCdNm']
+            }
+
+        return [refine(raw) for raw in grade_list]
+
+
+def crawl_major(username, password):
+    return ['False', '컴퓨터공학부', 'none']
+
+
+def crawl_same_replace(userid, password):
+    session = login(userid, password)
+    if session is None:
+        return None
+    with session as s:
+        same_url = 'https://shine.snu.ac.kr/uni/uni/cour/curr/findSameSubstSbjtInqStd.action'
+        replace_url = 'https://shine.snu.ac.kr/uni/uni/cour/curr/findSameSubstSbjtInqStdList.action'
+        params = {'cscLocale': 'ko_KR', 'strPgmCd': 'S030205'}
+        headers = {'Content-Type': 'application/extJs+sua; charset=UTF-8'}
+        payload = {
+            "SNU": {
+                "strSameSubstGrpFg": "",
+                "rowStatus": "insert",
+                "strDetaBussCd": "A0071",
+                "strSchyy": "2016",
+                "strShtmFg": "U000200002",
+                "strDetaShtmFg": "U000300002",
+                "strShtmDetaShtmFg": "U000200002U000300002",
+                "strBdegrSystemFg": "U000100001",
+                "strSbjtCd": "",
+                "strSbjtNm": ""
+            }
+        }
+        same_res = s.post(same_url, params=params, headers=headers, data=json.dumps(payload))
+        same_courses = same_res.json()['GRD_COUR102']
+        same = [{'code': c['sbjtCd'], 'group': c['sameSubstGrpNo']} for c in same_courses]
+
+        replace_res = s.post(replace_url, params=params, headers=headers, data=json.dumps(payload))
+        replace_courses = replace_res.json()['GRD_COUR102']
+        replace = [{'code_from': c['sbjtCd'], 'code_to': c['substSbjtCd']} for c in replace_courses]
+        return {'same': same, 'replace': replace}
