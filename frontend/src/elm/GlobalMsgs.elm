ff251086880f832ce855de14fe3a9b86957529e5
dnsdhrj <dnsdhrj123@gmail.com>
2017-11-25 23:23:56 +0900

    Vue setting - clear frontend

--- frontend/src/elm/GlobalMsgs.elm
+++
@@ -1,5 +0,0 @@
-module GlobalMsgs exposing (..)
-
-
-type GlobalMsg
-  = Loading Bool
--- sysconfcpus.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Running elm-make with sysconfcpus -n 2"
-
-/sysconfcpus/bin/sysconfcpus -n 2 elm-make-old "$@"
